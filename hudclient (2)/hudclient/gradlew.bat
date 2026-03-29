f2efe7e349e53a3e7f7bbf256c1608d85ea2be57
system3rtytwo-netizen <system3rtytwo@gmail.com>
2026-03-29 19:33:03 +0200

    Delete hudclient (2)/hudclient directory

--- hudclient (2)/hudclient/gradlew.bat
+++
@@ -1,5 +0,0 @@
-@rem Gradle startup script for Windows
-@if "%DEBUG%"=="" @echo off
-set APP_HOME=%~dp0
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
